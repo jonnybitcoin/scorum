9ca27b731bf5b39a12988f5f66b49c3e33667a5b
alexplevako <alexplevako@gmail.com>
2018-01-10 20:38:31 +0300

    update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo "==================================="
+printenv
+echo "==================================="
+ls -la
+echo "==================================="
 BRANCH_NAME="master"
 
 export IMAGE_NAME="scorum/blockchain:$BRANCH_NAME"
+
+echo "run docker"
 
 docker build -t=$IMAGE_NAME .
